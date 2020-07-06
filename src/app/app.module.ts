2afe616a21ea0bf39fa86f00048c3debb721c50e
syed <syed@empoweringsystems.com>
2020-07-06 20:05:27 +0530

    Added validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routing.module';
 
 import { AppComponent } from './app.component';
@@ -19,6 +19,7 @@ import { ProvidersComponent } from './components/providers/providers.component';
     BrowserModule,
     RouterModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule
   ],
--- src/app/components/provider/provider.component.ts
+++ src/app/components/provider/provider.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Provider } from 'src/app/model/providers';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PROVIDERS_LIST } from 'src/app/mock/providers';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-provider',
@@ -11,7 +12,22 @@ import { PROVIDERS_LIST } from 'src/app/mock/providers';
 export class ProviderComponent implements OnInit {
 
   provider: Provider;
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  providerForm: FormGroup;
+
+  constructor(private route: ActivatedRoute, private router: Router, private formBuilder: FormBuilder) {
+    this.providerForm = formBuilder.group({
+      serialNumber: ['', Validators.required],
+      name: ['', Validators.required],
+      licence: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', Validators.required],
+      address: ['', Validators.required],
+      created: ['', Validators.required],
+      status: ['', Validators.required]
+    });
+  }
+
+  get providerFormControls() { return this.providerForm.controls; }
 
   ngOnInit(): void {
     this.route.params.subscribe(param => {
@@ -19,16 +35,26 @@ export class ProviderComponent implements OnInit {
         this.provider = PROVIDERS_LIST.find((data: Provider) => data.serialNumber === Number(param.id));
       }
     });
+    this.setValues();
   }
 
-  closeDcoument() {
-    this.router.navigate(['providers']);
+
+  setValues(): void {
+    for (const control in this.providerFormControls) {
+      if (control) {
+        this.providerFormControls[control].setValue(this.provider[control]);
+      }
+    }
   }
 
-  onSubmit() {
-    const doc = PROVIDERS_LIST.filter((data: Provider) => data.serialNumber !== this.provider.serialNumber );
-    doc.push(this.provider);
+
+  onFormSubmit(): void {
+    const idx = PROVIDERS_LIST.findIndex((data: Provider) => data.serialNumber === this.providerForm.value.serialNumber);
+    PROVIDERS_LIST.splice(idx, 1, this.providerForm.value);
     this.closeDcoument();
   }
 
+  closeDcoument(): void {
+    this.router.navigate(['providers']);
+  }
 }
